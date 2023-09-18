3583613194107f015f2909848920fd33644a5819
White Night <1602699904@qq.com>
2023-09-18 14:47:31 +0800

    记得修改路径

---
+++ bat/关闭网易云音乐和WallpaperEngine.bat
@@ -0,0 +1,5 @@
+cd /d D:\Program Files (x86)\Netease\CloudMusic
+taskkill /f /im cloudmusic.exe
+cd /d D:\Program Files (x86)\Steam\steamapps\common\wallpaper_engine
+taskkill /f /im ui32.exe
+wallpaper32.exe -control stop

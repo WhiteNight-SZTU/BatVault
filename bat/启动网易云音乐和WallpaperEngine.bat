c4e0c32fa4a4fe3fc216b05b99e79900d5847f69
White Night <1602699904@qq.com>
2023-09-18 14:47:19 +0800

    优化
    为配合“关闭**”bat而进行了优化

--- bat/启动网易云音乐和WallpaperEngine.bat
+++ bat/启动网易云音乐和WallpaperEngine.bat
@@ -2,3 +2,4 @@ cd /d D:\Program Files (x86)\Netease\CloudMusic
 start cloudmusic.exe
 cd /d D:\Program Files (x86)\Steam\steamapps\common\wallpaper_engine
 start launcher.exe
+wallpaper32.exe -control play

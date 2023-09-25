aba5d5e0be881ad1466d5f8116329ab72f20c643
White Night <1602699904@qq.com>
2023-09-25 10:20:47 +0800

    update

--- bat/关闭网易云音乐和WallpaperEngine.bat
+++ bat/关闭网易云音乐和WallpaperEngine.bat
@@ -3,3 +3,4 @@ taskkill /f /im cloudmusic.exe
 cd /d D:\Program Files (x86)\Steam\steamapps\common\wallpaper_engine
 taskkill /f /im ui32.exe
 wallpaper32.exe -control stop
+taskkill /f /im wallpaper32.exe
--- bat/启动网易云音乐和WallpaperEngine.bat
+++ bat/启动网易云音乐和WallpaperEngine.bat
@@ -3,3 +3,4 @@ start cloudmusic.exe
 cd /d D:\Program Files (x86)\Steam\steamapps\common\wallpaper_engine
 start launcher.exe
 wallpaper32.exe -control play
+exit
